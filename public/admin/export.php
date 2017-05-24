64f7a097fc0e7863bfbd793895dcd5e4ba8e7c21
Youssef el Jaddaoui <d18322@edu.rocwb.nl>
2017-05-24 22:52:01 +0200

    Updated navbar

---
+++ public/admin/export.php
@@ -0,0 +1,10 @@
+<?php require '../templates/admheader.php'?>
+
+    <div class="main-content">
+
+        <!-- site's main content goes here -->
+
+
+    </div>
+
+<?php require '../templates/footer.php';
