20cb0b8a68ca63dfd028fad9d57e98c456c20ac2
Titson <tomaszt@hotmail.nl>
2017-05-29 01:13:27 +0200

    Added max team

--- app/addteam.php
+++ app/addteam.php
@@ -11,62 +11,67 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     require_once "connection.php";
     require_once("ValidateAddTeam.php");
 
-    $team = $_POST['teamname'];
+    $stmt = $db_conn->prepare("SELECT * FROM tbl_teams WHERE `deleted_at` IS NULL");
+    $stmt->execute();
+    $rezult = $stmt->rowCount();
 
-    $validate = new app\ValidateAddTeam();
-
-    if ($validate->validateteam($team) == true) {
-        $stmt = $db_conn->prepare("SELECT `name` FROM tbl_teams WHERE `name` = :name");
-        $stmt->execute(array("name" => $team));
-
-        if ($stmt->rowCount() == 0) {
-
-            $arr_bool = array();
-            $db_conn->beginTransaction();
-            try {
-                foreach($something as $some)
-                {
-                    // insert into database
-                }
-                $db_conn->commit();
-            } catch(PDOException $e)
-            {
-                echo $e->getMessage();
-                $db_conn->rollBack();
-            }
-
-            foreach ($_POST['p'] as $player) {
-                if ($validate->validateplayer($player)) {
-                    array_push($arr_bool, true);
-                } else {
-                        array_push($arr_bool, false);
-                    $_SESSION['error'] = "Je hebt geen geldige speler naam of id ingevoerd";
-                }
-            }
-            if ($validate->validateArray($arr_bool) == true)
-            {
-                $stmt = $db_conn->prepare("INSERT INTO tbl_teams (poule_id, `name`) VALUES (:poule_id, :name)");
-                $stmt->execute(array("poule_id" => 1, "name" => $team));
-
-                $stmt = $db_conn->prepare("SELECT id FROM tbl_teams WHERE `name` = :teamname");
-                $stmt->execute(array("teamname" => $team));
-                $teamname = $stmt->fetch();
-
-                foreach ($_POST['p'] as $player)
-                {
-                    $stmt = $db_conn->prepare("INSERT INTO tbl_players (student_id, team_id, first_name, last_name) VALUES (:student_id, :team_id, :first_name, :last_name)");
-                    $stmt->execute(array("student_id" => $player[0], "team_id" => $teamname[0], "first_name" => $player[1], "last_name" => $player[2]));
-                    $_SESSION['success'] = "Team Toegevoegd";
-                }
-            }
-        }
-        else
-        {
-            $_SESSION['error'] = "Deze team bestaat al.";
-        }
+    if ($rezult >= 16)
+    {
+        $_SESSION['error'] = "Je hebt maximale aantal teams bereikt";
     }
-    else{
-        $_SESSION['error'] = "Je hebt geen geldige teamnaam ingevoerd.";
+    else {
+
+        $team = $_POST['teamname'];
+
+        $validate = new app\ValidateAddTeam();
+
+        if ($validate->validateteam($team) == true) {
+            $stmt = $db_conn->prepare("SELECT `name` FROM tbl_teams WHERE `name` = :name");
+            $stmt->execute(array("name" => $team));
+
+            if ($stmt->rowCount() == 0) {
+
+                $arr_bool = array();
+                $db_conn->beginTransaction();
+                try {
+                    foreach ($something as $some) {
+                        // insert into database
+                    }
+                    $db_conn->commit();
+                } catch (PDOException $e) {
+                    echo $e->getMessage();
+                    $db_conn->rollBack();
+                }
+
+                foreach ($_POST['p'] as $player) {
+                    if ($validate->validateplayer($player)) {
+                        array_push($arr_bool, true);
+                    } else {
+                        array_push($arr_bool, false);
+                        $_SESSION['error'] = "Je hebt geen geldige speler naam of id ingevoerd";
+                    }
+                }
+                if ($validate->validateArray($arr_bool) == true) {
+                    $stmt = $db_conn->prepare("INSERT INTO tbl_teams (poule_id, `name`) VALUES (:poule_id, :name)");
+                    $stmt->execute(array("poule_id" => 1, "name" => $team));
+
+                    $stmt = $db_conn->prepare("SELECT id FROM tbl_teams WHERE `name` = :teamname");
+                    $stmt->execute(array("teamname" => $team));
+                    $teamname = $stmt->fetch();
+
+                    foreach ($_POST['p'] as $player) {
+                        $stmt = $db_conn->prepare("INSERT INTO tbl_players (student_id, team_id, first_name, last_name) VALUES (:student_id, :team_id, :first_name, :last_name)");
+                        $stmt->execute(array("student_id" => $player[0], "team_id" => $teamname[0], "first_name" => $player[1], "last_name" => $player[2]));
+                        $_SESSION['success'] = "Team Toegevoegd";
+                    }
+                }
+            } else {
+                $_SESSION['error'] = "Deze team bestaat al.";
+            }
+        }
+        else{
+            $_SESSION['error'] = "Je hebt geen geldige teamnaam ingevoerd.";
+        }
     }
     header("location: ../public/admin/addteam.php");
 }
