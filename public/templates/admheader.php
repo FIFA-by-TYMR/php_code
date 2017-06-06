d801fc117c8ffbfb14b93c4fd72c86f95edd6fa3
Fussia <D201653@edu.rocwb.nl>
2017-06-06 13:25:28 +0200

    updated admheader
    updated admheader

--- public/templates/admheader.php
+++ public/templates/admheader.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <title>Project Fifa</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/style.css">
 
@@ -15,7 +14,7 @@
 <header>
     <div class="container">
         <div class="logo">
-            <img src="../assets/img/logo.png" class="img-responsive center-block">
+            <img src="../assets/img/fifa-dev.png" class="img-responsive center-block">
         </div>
     </div>
 </header>
