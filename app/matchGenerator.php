63eed118ba8f29aebc4737fbd91821fcfa8d8695
Tomasz Tabis <tomaszt@hotmail.nl>
2017-06-14 12:02:02 +0200

    Updated match generator

--- app/matchGenerator.php
+++ app/matchGenerator.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require "connection.php";
 
 for ($poules = 1; $poules <= 4; $poules++){
@@ -13,11 +14,26 @@ for ($poules = 1; $poules <= 4; $poules++){
         array_push($arr_teams_id, $team['id']);
     }
 
+    $stmt = $db_conn->prepare("SELECT * FROM tbl_teams WHERE `poule_id` =$poules");
+    $stmt->execute();
+    $teams = $stmt->fetchAll();
+
     for ($i = 0; $i <= sizeof($arr_teams_id) - 1; $i++){
         for ($c = $i +1; $c <= sizeof($arr_teams_id) - 1; $c++){
             if ($i != $c){
-                $stmt = $db_conn->prepare("INSERT INTO tbl_matches (`team_id_a`, `team_id_b`) VALUES (:team_a, :team_b)");
-                $stmt->execute(array("team_a" => $arr_teams_id[$i], "team_b" => $arr_teams_id[$c]));
+                $stmt = $db_conn->prepare("SELECT * FROM tbl_matches WHERE `team_id_a` = $arr_teams_id[$i] AND `team_id_b` = $arr_teams_id[$c]");
+                $stmt->execute();
+                $matcheCheck = $stmt->rowCount();
+
+                if ($matcheCheck == 0)
+                {
+                    $stmt = $db_conn->prepare("INSERT INTO tbl_matches (`team_id_a`, `team_id_b`) VALUES (:team_a, :team_b)");
+                    $stmt->execute(array("team_a" => $arr_teams_id[$i], "team_b" => $arr_teams_id[$c]));
+                    $_SESSION['success'] = "Metches zijn gegenereerd";
+                }
+                else{
+                    $_SESSION['error'] = "Je hebt all matches gegenereerd";
+                }
             }
         }
     }
