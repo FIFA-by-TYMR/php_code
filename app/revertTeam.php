3c97ed2492cb566aedeef2aaf277309b9941c98c
Tomasz Tabis <tomaszt@hotmail.nl>
2017-06-16 09:35:34 +0200

    Updated revertTeam

--- app/revertTeam.php
+++ app/revertTeam.php
@@ -4,21 +4,27 @@ session_start();
 
 require "connection.php";
 
-$selectie = $_POST['select_catalog'];
+$stmt = $db_conn->prepare("SELECT * FROM tbl_teams WHERE `deleted_at` IS NULL");
+$stmt->execute();
+$deletedTeams = $stmt->rowCount();
 
-$stmt = $db_conn->prepare("SELECT `name` FROM tbl_teams WHERE `name` = :naam");
-$stmt->execute(array("naam" => $selectie));
-$result = $stmt->rowCount();
+if ($deletedTeams >= 16){
+    $_SESSION['error'] = "Je moet eerst een extra team verwijderen om team terug te zetten";
+}
+else {
+    $selectie = $_POST['select_catalog'];
 
-if ($result == 1){
-    $stmt = $db_conn->prepare("UPDATE tbl_teams SET deleted_at = NULL WHERE `name` = :naam");
+    $stmt = $db_conn->prepare("SELECT `name` FROM tbl_teams WHERE `name` = :naam");
     $stmt->execute(array("naam" => $selectie));
+    $result = $stmt->rowCount();
 
-    $_SESSION['success'] = "Team Teruggezet!";
-}
-else
-{
-    $_SESSION['error'] = "De gekozen team bestaat niet!";
-}
+    if ($result == 1) {
+        $stmt = $db_conn->prepare("UPDATE tbl_teams SET deleted_at = NULL WHERE `name` = :naam");
+        $stmt->execute(array("naam" => $selectie));
 
+        $_SESSION['success'] = "Team Teruggezet!";
+    } else {
+        $_SESSION['error'] = "De gekozen team bestaat niet!";
+    }
+}
 header("location: ../public/admin/admindex.php");
